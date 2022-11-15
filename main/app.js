a8d37c811ba40f856d979348c111473608c48f4b
Ioan1234 <101118809+Ioan1234@users.noreply.github.com>
2022-11-15 15:39:10 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,5 +1,32 @@
 function addTokens(input, tokens){
-    
+    if(typeof input==='string')
+    {
+        if(input.length>=6)
+        {
+            x = tokens.find(token => (typeof token.tokenName !== 'string'))
+            if (x === undefined) {
+                if (input.includes('...')) {
+                    out = tokens.find(token => (typeof token.tokenName === 'string'));
+                    x = input.replace('...','');
+                    a = x.concat('${'+out.tokenName+'}');
+                    return a;
+                }
+                else {
+                    return input;
+                }
+            }
+            else {
+                throw new Error('Invalid array format');
+            }
+        }
+        else {
+            throw new Error('Input should have at least 6 characters');
+        }
+
+    }
+    else {
+        throw new Error('Invalid input');
+    }
 }
 
 const app = {
